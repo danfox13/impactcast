ea0fd26e9d06b76c47e2a21b63d39d9937a65948
foxydan13 <danfox1305@gmail.com>
2017-06-20 12:45:12 +0100

    Adding new database connection

--- globals.js
+++ globals.js
@@ -1,4 +1,5 @@
 var globals = {};
 // globals.dburl = 'localhost:27017/impactcast';
-globals.dburl = 'mongodb://admin:admin@ds151049.mlab.com:51049/impactcast'
+//globals.dburl = 'mongodb://admin:admin@ds151049.mlab.com:51049/impactcast'
+globals.dburl = 'mongodb://admin:admin@ds129352.mlab.com:29352/reactif'
 module.exports = globals;
