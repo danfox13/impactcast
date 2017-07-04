4c153a79fcea398d19482f7df897dcdb9b0189b5
Greg Wolverson <greg.wolverson@capgemini.com>
2017-07-04 09:42:12 +0100

    Adding end-to-end search functionality for team search

--- client/src/containers/Team/TeamSearchResultsPage.js
+++ client/src/containers/Team/TeamSearchResultsPage.js
@@ -5,10 +5,37 @@ import React, {Component} from 'react';
 import TeamSearchResults from '../../components/Team/TeamSearchResults';
 
 export default class TeamSearchResultsPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            results: []
+        };
+
+        this.performSearch = this.performSearch.bind(this);
+    }
+
+    componentWillMount() {
+        this.performSearch();
+    }
+
+    performSearch() {
+        let url = `http://localhost:3001/searchTeams?teamName=${this.props.location.query.teamName}&resourceName=${this.props.location.query.resourceName}`;
+        fetch(url)
+            .then(response => response.json())
+            .then((result) => {
+                this.setState({
+                    results: result.results.results
+                });
+            })
+            .catch(err => console.log(err));
+    }
+
     render() {
         return (
             <div>
-                <TeamSearchResults />
+                <TeamSearchResults
+                    searchResults={this.state.results}
+                />
             </div>
         )
     }
