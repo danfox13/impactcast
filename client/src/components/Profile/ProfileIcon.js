14786f82f1ead63a6b430d9211a9965c63bfd2f4
Adam Khazi <adamkhazi@gmail.com>
2017-07-27 10:01:47 +0100

    profile icon component changed to react bootstrap

--- client/src/components/Profile/ProfileIcon.js
+++ client/src/components/Profile/ProfileIcon.js
@@ -1,16 +1,12 @@
 import React, {Component} from 'react';
+import {Media, Image} from 'react-bootstrap';
 
 export default class ProfileIcon extends Component {
 	render() {
-		let style = {
-		    width: 200,
-		    margin_left: 250
-
-		};
 		return (
-		<div className="media-left media-top">
-                    <img src="/img/avatar.png" className="img-circle" style={style}/>
-                </div>
+		<Media align="top">
+                    <Image width={250} src="/img/avatar.png" className="icon"/>
+                </Media>
 		)
 	}
 }
