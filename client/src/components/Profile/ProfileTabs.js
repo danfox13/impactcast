f80a732dc0261ab3a9c7f1f0853d748bfb9b23d8
Adam Khazi <adamkhazi@gmail.com>
2017-07-27 12:19:54 +0100

    profiletabs delete account tab content

--- client/src/components/Profile/ProfileTabs.js
+++ client/src/components/Profile/ProfileTabs.js
@@ -1,7 +1,18 @@
 import React, {Component} from 'react';
-import {Media, Row, Col, Panel} from 'react-bootstrap';
+import {InputGroup, Input,  FormControl, Form, FormGroup, Media, Row, Col, Panel, Nav, NavItem, Tabs, Tab} from 'react-bootstrap';
 
 export default class ProfileTabs extends Component {
+       constructor(props) {
+	        super();
+	        this.state = {
+	          // Takes active tab from props if it is defined there
+	          activeTab: props.activeTab || 1
+	        };
+	    
+	        // Bind the handleSelect function already here (not in the render function)
+	        this.handleSelect = this.handleSelect.bind(this);
+       }
+
 	render() {
 		return (
 		<Media.Body>
@@ -17,8 +28,43 @@ export default class ProfileTabs extends Component {
 			            <Panel className="panel-footer"><font size="4"></font></Panel>
 			    </Panel>
 			</Col>
+			<Col md={6}>
+
+		            <Tabs activeKey={this.state.activeTab} bsStyle="tabs" onSelect={this.handleSelect}>
+			        <Tab eventKey={1} title="Change Password">content </Tab>
+
+			        <Tab eventKey={2} title="Slack">content </Tab>
+
+			        <Tab eventKey={5} title="Delete Account">
+                                    <form action="/deleteMe" method="post" data-toggle="validator" role="form">
+					<FormGroup>
+					    <InputGroup>
+                                                <span className="input-group-addon">T</span>
+                                                <FormControl type="password" id="password" name="password" placeholder="Enter your password" required/>
+					    </InputGroup>
+					</FormGroup>
+					<FormControl type="submit" className="btn-danger btn-m btn-black" value="Permanently Delete Account"></FormControl>
+                                    </form>
+				</Tab>
+			    </Tabs>
+
+			</Col>
 		     </Row>
 		 </Media.Body>
 		)
 	}
-}
+
+	handleSelect(selectedTab) {
+	    // The active tab must be set into the state so that
+	    // the Tabs component knows about the change and re-renders.
+	    this.setState({
+	      activeTab: selectedTab
+	    });
+	  }
+	}
+
+
+
+
+
+
