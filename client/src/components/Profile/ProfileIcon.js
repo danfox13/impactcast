c4ca4c830aceffb79f68dba708adb1d9531c0cfc
Adam Khazi <adamkhazi@gmail.com>
2017-07-27 10:51:21 +0100

    Profile conversion to react bootstrap

--- client/src/components/Profile/ProfileIcon.js
+++ client/src/components/Profile/ProfileIcon.js
@@ -4,9 +4,9 @@ import {Media, Image} from 'react-bootstrap';
 export default class ProfileIcon extends Component {
 	render() {
 		return (
-		<Media align="top">
+		<Media.Left align="top">
                     <Image width={250} src="/img/avatar.png" className="icon"/>
-                </Media>
+                </Media.Left>
 		)
 	}
 }
--- client/src/components/Profile/ProfileTabs.js
+++ client/src/components/Profile/ProfileTabs.js
@@ -1,31 +1,24 @@
 import React, {Component} from 'react';
+import {Media, Row, Col, Panel} from 'react-bootstrap';
 
 export default class ProfileTabs extends Component {
 	render() {
-		let styles = {
-			width: 30
-		};
-
 		return (
-		<div className="media-body">
-                    <div className="row">
-
-                        <div className="col-md-6" >
-                            <div className="panel panel-custom2">
-                                <div className="panel-body"><font color="#465a64" size="5"><strong>Email Address</strong></font></div>
-                                <div className="panel-footer"><font size="4"></font></div>
-                            </div>
-                            <div className="panel panel-custom2" >
-                                <div className="panel-body">
-                                    <img style={styles} alt="" src="/img/slackLogo.svg" className="img-circle" />
+		<Media.Body>
+		    <Row>
+			<Col md={6}>
+			    <Panel className="panel-custom2">
+                                <font color="#465a64" size="5"><strong>Email Address</strong></font>
+                                <Panel className="panel-footer"><font size="4"></font></Panel>
+			    </Panel>
+			    <Panel className="panel-custom2">
+                                    <img width={20} alt="" src="/img/slackLogo.svg" className="img-circle" />
                                     <font color="#465a64" size="5"><strong>Slack Handle</strong></font>
-                                </div>
-                                <div className="panel-footer"><font size="4"></font></div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
+			            <Panel className="panel-footer"><font size="4"></font></Panel>
+			    </Panel>
+			</Col>
+		     </Row>
+		 </Media.Body>
 		)
 	}
 }
